7491c6cdbdd1106a95cf44a726880fa3a1edb6c2
Luigi Malaguti <malaguti.luigi@gmail.com>
2026-07-22 16:48:34 +0200

    sort_int: v03

--- packages/sort_int/src/main.cpp
+++ packages/sort_int/src/main.cpp
@@ -20,14 +20,46 @@ struct vector {
         data_ = NULL;
     }
 
+    vector(size_t capacity) {
+        size_ = 0;
+        capacity_ = capacity;
+        data_ = new int32_t[capacity];
+    }
+
+    vector(const vector &other) {
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        data_ = new int32_t[capacity_];
+        for (size_t index = 0; index < size_; index++) {
+            data_[index] = other.data_[index];
+        }
+    }
+
     ~vector() {
-        free(data_);
+        delete[] data_;
+    }
+
+    vector &operator=(const vector &rhs) {
+        if (this != &rhs) {
+            size_ = rhs.size_;
+            capacity_ = rhs.capacity_;
+            delete[] data_;
+            data_ = new int32_t[capacity_];
+            for (size_t index = 0; index < size_; index++) {
+                data_[index] = rhs.data_[index];
+            }
+        }
+        return *this;
     }
 
     void push_back(int32_t number) {
         if (size_ >= capacity_) {
             size_t temp_capacity = capacity_ == 0 ? 1 : capacity_ * 2;
-            int32_t *temp_data = (int32_t *)realloc(data_, temp_capacity * sizeof(int32_t));
+            int32_t *temp_data = new int32_t[temp_capacity];
+            for (size_t index = 0; index < size_; index++) {
+                temp_data[index] = data_[index];
+            }
+            delete[] data_;
             data_ = temp_data;
             capacity_ = temp_capacity;
         }
@@ -47,7 +79,36 @@ struct vector {
         assert(index < size_);
         return data_[index];
     }
+
+    bool empty() const {
+        return size_ == 0;
+    }
 };
+
+vector read_from_file(const char *filename, size_t capacity) {
+    vector vec(capacity);
+    FILE *file = fopen(filename, "r");
+    if (file != NULL) {
+        int32_t number;
+        while (fscanf(file, "%d", &number) == 1) {
+            vec.push_back(number);
+        }
+        fclose(file);
+    }
+    return vec;
+}
+
+bool write_to_file(const char *filename, const vector &vec) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        return false;
+    }
+    for (size_t index = 0; index < vec.size(); index++) {
+        fprintf(file, "%d\n", vec.at(index));
+    }
+    fclose(file);
+    return true;
+}
 
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -58,33 +119,20 @@ int main(int argc, char **argv) {
     const char *input_filename = argv[1];
     const char *output_filename = argv[2];
 
-    FILE *input_file = fopen(input_filename, "r");
-    if (input_file == NULL) {
+    vector vec;
+    vec = read_from_file(input_filename, 10);
+    if (vec.empty() == true) {
         printf("Error: Could not open file %s\n", input_filename);
         return EXIT_FAILURE;
     }
 
-    vector *vec = new vector();
+    vec.sort();
 
-    int32_t number;
-    while (fscanf(input_file, "%d", &number) == 1) {
-        vec->push_back(number);
-    }
-    fclose(input_file);
-
-    vec->sort();
-
-    FILE *output_file = fopen(output_filename, "w");
-    if (output_file == NULL) {
+    bool result = write_to_file(output_filename, vec);
+    if (result == false) {
         printf("Error: Could not open file %s\n", output_filename);
         return EXIT_FAILURE;
     }
 
-    for (size_t index = 0; index < vec->size(); index++) {
-        fprintf(output_file, "%d\n", vec->at(index));
-    }
-    fclose(output_file);
-
-    delete vec;
     return EXIT_SUCCESS;
 }
